211a327117efa0f7f5b05ffaac5c0b71683bf497
Frederick Ngo <frederick.ngo@rogers.com>
2018-01-26 02:16:28 -0500

    Add hash libs. Some testing and improved functionality.

--- main.js
+++ main.js
@@ -1,49 +1,56 @@
-// requires a hash function library
+const SHA256 =  require('crypto-js/sha256')
 class Block{
-  constructor(time, data, previousHash = ''){
-    this.time = time;
+  constructor(index, timestamp, data, previousHash = '', hash){
+    this.index = index;
+    this.timestamp = timestamp;
     this.data = data;
     this.previousHash = previousHash;
-    this.hash = hash;
-    this.nonce = nonce;
+    this.hash = this.calcHash();
+    this.nonce = 0;
   }
   calcHash(){
-    // pls import a hash lib
-    return someHash(this.time + this.previousHash + this.nonce + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.timestamp + this.previousHash + this.nonce + JSON.stringify(this.data)).toString();
   }
   mineBlock(zeroes){
     while(this.hash.substring(0,zeroes) != Array(zeroes + 1).join("0")){
       this.nonce++;
       this.hash = this.calcHash();
     }
+    console.log("Block Mined: " + this.hash)
   }
 }
 class Blockchain{
   constructor(){
-    this.chain = [this.createFirstBlock()];
-    this.zeroes = 2;
+    this.chain = [this.createGenesisBlock()];
+    this.zeroes = 3;
   }
-  createFirstBlock(){
-    return new Block("01/03/2018", "first block", "0");
+  createGenesisBlock(){
+    return new Block(0,"01/03/2018", "genesis block", "1st one");
   }
   getLastBlock(){
     return this.chain[this.chain.length - 1];
   }
   addBlock(block){
     block.previousHash = this.getLastBlock().hash;
-    block.hash.mineBlock(this.zeroes);
+    block.mineBlock(this.zeroes);
     // Can't do this in real life: need to check for validity
     // will try to add this in the future
     this.chain.push(block);
   }
 
   isChainValid(){
-    for(let i = 1; i <this.chain.length; i++){
+    for(let i = 1; i < this.chain.length; i++){
       const currBlock = this.chain[i];
       const prevBlock = this.chain[i-1];
-    }
+    if(prevBlock.index + 1 != currBlock.index) return false;
     if(currBlock.hash != currBlock.calcHash()) return false;
     if(currBlock.previousHash != prevBlock.hash) return false;
-
+    }
+    return true;
   }
 }
+
+let test = new Blockchain();
+test.addBlock(new Block(1,"01/02/2018","first test","data?"));
+test.addBlock(new Block(2,"01/03/2018","block2",{ amount: 2 }));
+console.log(JSON.stringify(test, null, 4));
